da211591e1840fd531c1681b88378bea1d4c850c
koteyka121991 <106138744+koteyka121991@users.noreply.github.com>
2022-10-16 15:36:38 +0300

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,7 @@ const Header = () => {
             </div>
             <div className="header-search">
                 <input type="search" placeholder='Поиск'></input>
-                  привет мир
-                  ghbdtn vbh
+                
             </div>
         </div>
     </header>
