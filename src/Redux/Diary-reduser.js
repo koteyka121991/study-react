d44aa2216534394798bfa5c79e13ac31fa983235
koteyka121991 <106138744+koteyka121991@users.noreply.github.com>
2022-11-07 14:17:39 +0300

    added addition of approaches and repetitions to the diary

--- src/Redux/Diary-reduser.js
+++ src/Redux/Diary-reduser.js
@@ -1,5 +1,9 @@
 const ADD_WEIGHT = 'ADD-WEIGHT';
 const UPDATE_NEW_WEIGHT_TEXT = 'UPDATE-NEW-WEIGHT-TEXT';
+const ADD_SET = 'ADD-SET';
+const UPDATE_NEW_SET_TEXT = 'UPDATE-NEW-SET-TEXT';
+const ADD_APPROACHES = 'ADD-APPROACHES';
+const UPDATE_NEW_APPROACHES_TEXT = 'UPDATE-NEW-APPROACHES-TEXT';
 let initialState = {
     exerciseNameDate: [
         { id: 1, exercise: "Присяд" }
@@ -8,32 +12,65 @@ let initialState = {
         { id: 1, date: "29 суббота" }
     ],
     workoutWeight: [
-        { id: 1, weight: 22}
-    ],     
+        { id: 1, weight: 22 }
+    ],
     workoutSet: [
-        { id: 1, set: 1}
-    ],        
+        { id: 1, set: 1 }
+    ],
     workoutApproaches: [
         { id: 1, approaches: 3 }
     ],
-    newWorkoutWeightText: ""
+    newWorkoutWeightText: "",
+    newWorkoutSetText: "",
+    newWorkoutApproachesText: ""
 }
-const diaryReduser =(state=initialState, action) => {
-    if (action.type === ADD_WEIGHT) {
-        let text = state.newWorkoutWeightText;
-        state.newWorkoutWeightText = '';
-        state.workoutWeight.push( { id: 2, weight: text},);
-       
-    } else if (action.type === UPDATE_NEW_WEIGHT_TEXT) {
-        state.newWorkoutWeightText = action.text;
-       
+const diaryReduser = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_WEIGHT:
+            let textWeight = state.newWorkoutWeightText;
+            state.newWorkoutWeightText = '';
+            state.workoutWeight.push({ id: 2, weight: textWeight });
+            return state;
+        case UPDATE_NEW_WEIGHT_TEXT:
+            state.newWorkoutWeightText = action.text;
+            return state;
+        case ADD_SET:
+            let textSet = state.newWorkoutSetText;
+            state.newWorkoutSetText = '';
+            state.workoutSet.push({ id: 2, set: textSet });
+            return state;
+        case UPDATE_NEW_SET_TEXT:
+            state.newWorkoutSetText = action.text;
+            return state;
+        case ADD_APPROACHES:
+            let textApproaches = state.newWorkoutApproachesText;
+            state.newWorkoutApproachesText = '';
+            state.workoutApproaches.push({ id: 1, approaches: textApproaches });
+            return state;
+        case UPDATE_NEW_APPROACHES_TEXT:
+            state.newWorkoutApproachesText = action.text;
+            return state;
+        default:
+            return state;
     }
-    return state;
-    }
-    export const addWeightCreator = () => ({
-        type: ADD_WEIGHT
-    })
-    export const updateNewWeightTextCreator = (text) => ({
-        type: UPDATE_NEW_WEIGHT_TEXT, text: text
-    })
-    export default diaryReduser;
+}
+export const addWeightCreator = () => ({
+    type: ADD_WEIGHT
+})
+export const updateNewWeightTextCreator = (text) => ({
+    type: UPDATE_NEW_WEIGHT_TEXT, text: text
+})
+export const addSetCreator = () => ({
+    type: ADD_SET
+})
+export const updateNewSetTextCreator = (text) => ({
+    type: UPDATE_NEW_SET_TEXT, text: text
+})
+export const addApproachesCreator = () => ({
+    type: ADD_APPROACHES
+})
+export const updateNewApproachesTextCreator = (text) => ({
+    type: UPDATE_NEW_APPROACHES_TEXT, text: text
+})
+
+export default diaryReduser;
--- src/pages/Diary/Diary.jsx
+++ src/pages/Diary/Diary.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addWeightCreator, updateNewWeightTextCreator } from '../../Redux/Diary-reduser';
+import { addApproachesCreator, addSetCreator, addWeightCreator, updateNewApproachesTextCreator, updateNewSetTextCreator, updateNewWeightTextCreator } from '../../Redux/Diary-reduser';
 import Approaches from './components/Approaches';
 import ExerciseName from './components/ExerciseName';
 import Left from './components/Left';
@@ -15,7 +15,8 @@ const Diary = (props) => {
     let exerciseNameElements = state.exerciseNameDate.map(el => <ExerciseName id={el.id} exercise={el.exercise} />);
     let approachesElements = state.workoutApproaches.map(el => <Approaches id={el.id} approaches={el.approaches} />);
     let newWorkoutWeightText = state.newWorkoutWeightText;
-
+    let newWorkoutSetText = state.newWorkoutSetText;
+    let newWorkoutApproachesText = state.newWorkoutApproachesText;
     let onSendTextWeight = () => {
         props.store.dispatch(addWeightCreator());
     }
@@ -23,6 +24,21 @@ const Diary = (props) => {
         let text = event.target.value;
         props.store.dispatch(updateNewWeightTextCreator(text));
     }
+    let onSendTextSet = () => {
+        props.store.dispatch(addSetCreator());
+    }
+    let onNewTextSetChage = (event) => {
+        let text = event.target.value;
+        props.store.dispatch(updateNewSetTextCreator(text));
+    }
+    let onSendTextApproaches = () => {
+        props.store.dispatch(addApproachesCreator());
+    }
+    let onNewTextApproachesChage = (event) => {
+        let text = event.target.value;
+        props.store.dispatch(updateNewApproachesTextCreator(text));
+    }
+
 
     return (
         <>
@@ -33,17 +49,24 @@ const Diary = (props) => {
                         {exerciseNameElements}
                         <div>{weightElements}
                             <div>
-                            <div><textarea onChange={onNewTextWeightChage} value={newWorkoutWeightText} placeholder='text of the exercise'></textarea></div>
+                                <div><textarea onChange={onNewTextWeightChage} value={newWorkoutWeightText} placeholder='text of the exercise'></textarea></div>
                                 <div><button onClick={onSendTextWeight}>send</button></div>
-                            </div></div>
-
+                            </div>
+                        </div>
                         <div>{setElements}
+                        <div>
+                                <div><textarea onChange={onNewTextSetChage} value={newWorkoutSetText} placeholder='text of the exercise'></textarea></div>
+                                <div><button onClick={onSendTextSet}>send</button></div>
+                            </div>
                         </div>
                         {approachesElements}
-                        <div className='right'>
-
+                        <div>
+                                <div><textarea onChange={onNewTextApproachesChage} value={newWorkoutApproachesText} placeholder='text of the exercise'></textarea></div>
+                                <div><button onClick={onSendTextApproaches}>send</button></div>
+                            </div>
                         </div>
-                    </div>
+                        <div className='right'>
+                        </div>                   
                 </li>
             </ul>
         </>
