ba5a855d67e84d464ab9fd94c9ebf047c94469ee
koteyka121991 <106138744+koteyka121991@users.noreply.github.com>
2022-11-13 22:57:00 +0300

    Separation of the user file into presentation and container. the component. Moved the ajax request to the container

--- src/pages/Users/UsersContainer.js
+++ src/pages/Users/UsersContainer.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { followActionCreator, setCurrentPageActionCreator, setTotalUsersCountActionCreator, setUsersActionCreator, unfollowActionCreator } from '../../Redux/Users-reduser';
+import axios from "axios";
 import Users from './Users';
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+    // onPageChanged = (pageNumber) => {
+    //     this.props.setCurrentPage(pageNumber);
+    //     axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+    //         this.props.setUsers(response.data.items);
+    //     });
+    // чтобы взять из пропсов 
+    // }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}`).then(response => {
+            this.props.setUsers(response.data.items);
+            
+        });
+    }
+    render() {       
+        return <Users totalUsersCount={this.props.totalUsersCount}
+        pageSize={this.props.pageSize}
+        currentPage={this.props.currentPage} 
+         onPageChanged={this.onPageChanged}
+         users={this.props.users}
+         follow={this.props.follow} 
+         unfollow={this.props.unfollow}  /> 
+    }
+}
+
 let mapStateToProps =(state) => {
     return {
         users: state.usersPage.users,
@@ -32,6 +66,5 @@ let mapDispatchToProps=(dispatch) => {
     }
 
 }
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
-export default UsersContainer;
